5cba5f488dc1b3c505d3abb874bf3d413694fe2b
sofiascarpetta <ssofiascarpetta@gmail.com>
2023-09-09 00:33:42 -0300

    tercer commmit

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -31,4 +31,29 @@ return (
  )
 }
 
+// const getProductos = new Promise((resolve, reject) => {
+//   if (productos.length > 0) {
+//     setTimeout(() => {
+//       resolve(productos);
+//     }, 2000);
+//   } else {
+//     reject(new Error("No hay datos"));
+//   }
+// });
+
+// getProductos
+// .then((res) =>{
+// })
+// .catch((error) =>{
+//   console.log(error);
+// })
+
+// return (
+//   <>
+//     <ItemDetail productos={productos}
+//     />
+//   </>
+// );
+
+
 export default ItemDetailContainer
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -25,4 +25,30 @@ const ItemListContainer = () => {
   )
 }
 
+// const getProductos = new Promise((resolve, reject) => {
+//   if (productos.length > 0) {
+//     setTimeout(() => {
+//       resolve(productos);
+//     }, 2000);
+//   } else {
+//     reject(new Error("No hay mas datos"));
+//   }
+// });
+
+// getProductos
+//   .then((res) => {})
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+
+// const productosFiltrados = productos.filter((producto) => producto.categoria === categoria)
+
+// return (
+
+//     categoria ? <ItemList productos={productosFiltrados} /> : <ItemList productos={productos} />
+
+// );
+
+
 export default ItemListContainer
